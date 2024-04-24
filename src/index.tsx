8c53eda3c7eb63381ba8243bf32a35307b032a02
unknown <thgeee@naver.com>
2024-04-25 02:57:21 +0900

    API호출 완료

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-import { darkTheme } from "./theme";
+import { darkTheme } from "./theme/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
