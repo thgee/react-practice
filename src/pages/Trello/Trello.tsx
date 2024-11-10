97f3452b714593a828437e5bd0e249086dd24aca
thgee <102576089+thgee@users.noreply.github.com>
2024-11-10 16:40:25 +0900

    feat: 쓰레기통 UI 구현

--- src/pages/Trello/Trello.tsx
+++ src/pages/Trello/Trello.tsx
@@ -2,8 +2,9 @@ import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import { useEffect } from "react";
 import { todoState } from "../../atoms";
-import { Boards, Wrapper } from "./styles";
+import { Boards, TrashCan, Wrapper } from "./styles";
 import { Board } from "../../components/Boards/Board";
+import { FaTrashAlt } from "react-icons/fa";
 
 export const Trello = () => {
   const [todos, setTodos] = useRecoilState(todoState);
@@ -52,6 +53,7 @@ export const Trello = () => {
             />
           ))}
         </Boards>
+        <TrashCan />
       </Wrapper>
     </DragDropContext>
   );
--- src/pages/Trello/styles.ts
+++ src/pages/Trello/styles.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { FaTrashAlt } from "react-icons/fa";
 
 export const Wrapper = styled.div`
   display: flex;
@@ -8,10 +9,21 @@ export const Wrapper = styled.div`
 
   padding: 60px 0;
 `;
+export const TrashCan = styled(FaTrashAlt)`
+  position: absolute;
+  top: 20px;
+  right: 30px;
+  font-size: 3.3rem;
+  color: #fff;
+  padding: 6px;
+  border-radius: 100px;
+  border: 2.4px solid #fff;
+`;
 export const Boards = styled.div`
   display: flex;
   width: 90%;
   justify-content: center;
+  margin-top: 40px;
 
   min-width: 1000px;
   flex-wrap: wrap;
