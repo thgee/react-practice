617af208287aaa5bfdd30b519b1a6532f936810f
thgee <102576089+thgee@users.noreply.github.com>
2024-11-20 18:28:33 +0900

    feat: 보드 내 순서변경 로직 수정

--- src/pages/Trello/Trello.tsx
+++ src/pages/Trello/Trello.tsx
@@ -32,7 +32,6 @@ export const Trello = () => {
 
   const onDragEnd = ({ destination, source }: DropResult) => {
     if (!destination) return;
-
     // 쓰레기통으로 이동
     // if (destination.droppableId === "trashCan") {
     //   setBoards((allBoards) => {
@@ -45,31 +44,42 @@ export const Trello = () => {
     //   return;
     // } else
     {
-      // 보드 내 이동
-      // setBoards((allBoards) => {
-      //   // 같은 보드 내에서 이동시
-      //   if (source.droppableId === destination.droppableId) {
-      //     const board = [...allBoards[source.index].todos];
-      //     const task = board[source.index];
-      //     board.splice(source.index, 1);
-      //     board.splice(destination.index, 0, task);
-      //     const editedBoardData = { ...allBoards, [source.droppableId]: board };
-      //     localStorage.setItem("boardsData", JSON.stringify(editedBoardData));
-      //     return editedBoardData;
-      //   }
-      //   const sourceBoard = [...allBoards[source.droppableId]]; // 출발배열
-      //   const destBoard = [...allBoards[destination.droppableId]]; // 도착배열
-      //   const task = sourceBoard[source.index];
-      //   sourceBoard.splice(source.index, 1); // 원래보드에 선택된 카드 삭제
-      //   destBoard.splice(destination.index, 0, task); // 옮긴 보드에 카드 추가
-      //   const editedBoardData = {
-      //     ...allBoards,
-      //     [destination.droppableId]: destBoard,
-      //     [source.droppableId]: sourceBoard,
-      //   };
-      //   localStorage.setItem("boardsData", JSON.stringify(editedBoardData));
-      //   return editedBoardData;
-      // });
+      setBoards((allBoards) => {
+        // 보드 내 이동
+        if (source.droppableId === destination.droppableId) {
+          const copyAllBoards = [...allBoards];
+          // 현재 보드의 idx를 찾아야 함
+          const targetBoardIdx = copyAllBoards.findIndex(
+            (board) => board.boardName === source.droppableId
+          );
+
+          // 현재보드의 투두에 들어있는 값들을 복사
+          const targetBoard = { ...copyAllBoards[targetBoardIdx] };
+          const targetBoardTodos = [...targetBoard.todos];
+          const task = targetBoardTodos[source.index]; // 현재 잡고있는 todo
+          // todo 순서바꾸기
+          targetBoardTodos.splice(source.index, 1);
+          targetBoardTodos.splice(destination.index, 0, task);
+
+          targetBoard.todos = targetBoardTodos;
+          copyAllBoards.splice(targetBoardIdx, 1, targetBoard);
+          localStorage.setItem("boardsData", JSON.stringify(copyAllBoards));
+          return copyAllBoards;
+        }
+        // 보드 간 이동
+        // const sourceBoard = [...allBoards[source.droppableId]]; // 출발배열
+        // const destBoard = [...allBoards[destination.droppableId]]; // 도착배열
+        // const task = sourceBoard[source.index];
+        // sourceBoard.splice(source.index, 1); // 원래보드에 선택된 카드 삭제
+        // destBoard.splice(destination.index, 0, task); // 옮긴 보드에 카드 추가
+        // const editedBoardData = {
+        //   ...allBoards,
+        //   [destination.droppableId]: destBoard,
+        //   [source.droppableId]: sourceBoard,
+        // };
+        // localStorage.setItem("boardsData", JSON.stringify(editedBoardData));
+        // return editedBoardData;
+      });
     }
   };
 
