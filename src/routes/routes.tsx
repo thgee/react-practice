13595307e24f958c8cb7b304ae28978bdfec632f
thgee <102576089+thgee@users.noreply.github.com>
2024-12-06 12:53:23 +0900

    build: BaseUrl 수정

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,8 +3,13 @@ import App from "../App";
 import { Trello } from "../pages/Trello/Trello";
 import { Framer } from "../pages/Framer/Framer";
 
-export const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/trello", element: <Trello /> },
-  { path: "/framer", element: <Framer /> },
-]);
+export const router = createBrowserRouter(
+  [
+    { path: "/", element: <App /> },
+    { path: "/trello", element: <Trello /> },
+    { path: "/framer", element: <Framer /> },
+  ],
+  {
+    basename: import.meta.env.BASE_URL,
+  }
+);
