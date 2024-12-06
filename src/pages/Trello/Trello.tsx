8a5f37acda3678c21ae788bbabe3493ed0e8d299
thgee <102576089+thgee@users.noreply.github.com>
2024-12-06 14:02:12 +0900

    deploy test

--- src/pages/Trello/Trello.tsx
+++ src/pages/Trello/Trello.tsx
@@ -15,7 +15,6 @@ import { Board } from "../../components/Trello/Board/Board";
 import { TrashCan } from "../../components/Trello/TrashCan/TrashCan";
 import { CancelBtn, OkBtn } from "../../components/Trello/Board/styles";
 import { useForm } from "react-hook-form";
-import Draggable from "react-draggable";
 
 interface IBoardForm {
   boardName: string;
