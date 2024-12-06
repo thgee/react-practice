c5b7a7b39523fbdf33d1dfa3246b1ab7a232fd21
thgee <102576089+thgee@users.noreply.github.com>
2024-12-06 13:11:36 +0900

    build: BaseUrl 수정

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,13 +3,8 @@ import App from "../App";
 import { Trello } from "../pages/Trello/Trello";
 import { Framer } from "../pages/Framer/Framer";
 
-export const router = createBrowserRouter(
-  [
-    { path: "/", element: <App /> },
-    { path: "/trello", element: <Trello /> },
-    { path: "/framer", element: <Framer /> },
-  ],
-  {
-    basename: `/react-practice`,
-  }
-);
+export const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/trello", element: <Trello /> },
+  { path: "/framer", element: <Framer /> },
+]);
