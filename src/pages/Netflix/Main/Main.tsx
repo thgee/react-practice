e3ad62f520d9af05c7eb05f5fc6e65e3216e0625
thgee <102576089+thgee@users.noreply.github.com>
2024-12-26 15:28:18 +0900

    feat: movieModal UI, 애니메이션 구현

--- src/pages/Netflix/Main/Main.tsx
+++ src/pages/Netflix/Main/Main.tsx
@@ -7,6 +7,7 @@ import {
 import { getImgPath } from "../../../utils/utils";
 import { AnimatePresence, motion, Variants } from "motion/react";
 import { useState } from "react";
+import { useMatch, useNavigate } from "react-router-dom";
 
 const sliderVariant: Variants = {
   initial: { x: "calc(100vw + 6px)" },
@@ -15,11 +16,17 @@ const sliderVariant: Variants = {
 };
 
 export const Main = () => {
+  const match = useMatch(`/netflix/:movieId`);
+  const navigate = useNavigate();
   const [sliderIdx, setSliderIdx] = useState(0);
+
   const { data: movieList } = useQuery<IGetPlayingMovies>({
     queryKey: ["movie", "nowPlaying"],
     queryFn: apiGetPlayingMovies,
   });
+  const toggleMovieModal = (movieId: number) => {
+    navigate(`/netflix/${movieId}`);
+  };
   return (
     <MainContainer>
       <Banner
@@ -48,20 +55,70 @@ export const Main = () => {
               .slice(sliderIdx * 6, (sliderIdx + 1) * 6)
               .map((it) => (
                 <SliderItem
+                  layoutId={"" + it.id}
+                  onClick={() => toggleMovieModal(it.id)}
+                  whileHover={{
+                    scale: 1.6,
+                    y: -80,
+                    zIndex: 1,
+                    transition: { delay: 0.3 },
+                  }}
+                  animate={{ zIndex: 0, transition: { delay: 0.3 } }}
                   key={it.id}
-                  imgSrc={getImgPath(it.backdrop_path!, "w300")}
-                />
+                  imgSrc={getImgPath(it.backdrop_path!, "w500")}
+                >
+                  <TitleBox> {it.title}</TitleBox>
+                </SliderItem>
               ))}
           </Row>
         </AnimatePresence>
       </Slider>
+      {match && <MovieModal movieId={match.params.movieId!} />}
     </MainContainer>
   );
 };
 
+export const MovieModal = ({ movieId }: { movieId: string }) => {
+  const navigate = useNavigate();
+  return (
+    <Outlay
+      onClick={() => {
+        navigate("/netflix");
+      }}
+    >
+      <MovieModalContainer
+        exit={{ scale: 3, color: "red" }}
+        layoutId={"" + movieId}
+      ></MovieModalContainer>
+    </Outlay>
+  );
+};
+
+const Outlay = styled(motion.div)`
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+`;
+const MovieModalContainer = styled(motion.div)`
+  width: 800px;
+  height: 500px;
+  position: fixed;
+  top: 100px;
+  background-color: white;
+  left: 0;
+  right: 0;
+  margin: auto;
+  border-radius: 16px;
+`;
+
+///////////////////////////////////
 const MainContainer = styled.div`
   background-color: black;
+  height: fit-content;
 `;
+
 const Banner = styled.div<{ img: string }>`
   padding: 60px;
   height: 100vh;
@@ -85,26 +142,47 @@ const Overview = styled.div`
 
 const Slider = styled.div`
   position: relative;
-  margin: 0 70px;
-  height: 200px;
-  overflow-x: hidden;
-  display: flex;
+  overflow: hidden;
   align-items: center;
+  height: 300px;
+  top: -200px;
 `;
 const Row = styled(motion.div)`
   position: absolute;
-  left: 0;
+  overflow: visible;
+  bottom: 0;
   width: 100%;
+  padding: 0px 70px;
   display: flex;
   gap: 10px;
 `;
 
-const SliderItem = styled.div<{ imgSrc: string }>`
+const SliderItem = styled(motion.div)<{ imgSrc: string }>`
   flex-grow: 1;
-  height: 160px;
+  aspect-ratio: 1.5/1;
   background-image: url(${(p) => p.imgSrc});
   background-size: cover;
   background-position: center;
   border-radius: 4px;
+  position: relative;
+
   cursor: pointer;
+  :first-child {
+    transform-origin: left;
+  }
+  :last-child {
+    transform-origin: right;
+  }
+`;
+
+const TitleBox = styled(motion.div)`
+  height: 30px;
+  color: #fff;
+  background-color: #222;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
 `;
